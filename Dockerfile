2ea811b92d4d230615cb1d86b857f7e44eb72d6c
peter.fercak <peter.fercak@visma.com>
2023-02-22 15:47:02 +0100

    update port number

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,5 @@ RUN pip3 install -r requirements.txt
 
 COPY . /app
 
+EXPOSE 8080
 CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -147,4 +147,4 @@ def api_delete(id):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
