341a302290706b2b1980f4aea2f7b8ed5e78e79e
peter.fercak <peter.fercak@visma.com>
2023-02-24 13:30:17 +0100

    trying kubernetes apply commands

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
-from flask import Flask, redirect
+"""
+Recruitment assignment for Site Reliability Engineers
+"""
+
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/<int:int_id>', methods=['GET'])
-def show_post(int_id):
-    if type(int_id) is int:
-        # return "Correct"
-        return "This is not expected response - dummy app has not been requested."
-    return redirect('/error')
+@app.route('/')
+# Homepage endpoint
+def index():
+    return "You are on homepage."
 
 
-@app.route('/error', methods=['GET'])
-def incorrect_param():
+@app.route('/error')
+# Error endpoint
+def error():
     return "Incorrect type of parameter"
+
+
+@app.route('/<number>')
+# Mime type endpoint
+def mime_type(number):
+    if number.isnumeric():
+        return "This is not expected response - dummy app has not been requested."
+    return redirect(url_for('error'))
+
+
+@app.route('/health')
+def health():
+    return "OK"
 
 
 if __name__ == '__main__':
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
 def test_incorrect_param(app, client):
     del app
-    res = client.get('/error')
+    res = client.get('/')
     assert res.status_code == 200
